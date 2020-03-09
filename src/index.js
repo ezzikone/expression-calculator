3be9a9518b79b83a7d2a2ec125cebd92a2e696a6
ezzikone <47362010+ezzikone@users.noreply.github.com>
2020-03-09 21:59:29 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ function eval() {
     // Do not use eval!!!
     return;
 }
-
 function expressionCalculator(expr) {
-    // write your solution here
+    let count = 0;
+    expr.split('').forEach((item, index) => {
+        if(item == '('){
+            count++;
+        }
+        if(item == ')'){
+            count--;
+        }
+        if (item === "/" && expr.split('')[index+2] === "0"){
+            throw new Error("TypeError: Division by zero.");
+        }
+    });
+    if(count != 0){
+        throw new Error ('ExpressionError: Brackets must be paired');
+    }
+    else{
+        let funcResult = new Function(`return ${expr}`);
+        return funcResult();
+    }
 }
-
 module.exports = {
     expressionCalculator
 }
